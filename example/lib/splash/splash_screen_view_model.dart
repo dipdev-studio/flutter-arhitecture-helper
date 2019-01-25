33b65ddaa35a7892cc3de4e1a51af991fe880fe8
Dimoshka <dmytro.prylutskyi@gmail.com>
2019-01-26 00:05:24 +0200

    fix

--- example/lib/splash/splash_screen_view_model.dart
+++ example/lib/splash/splash_screen_view_model.dart
@@ -3,6 +3,9 @@ import 'package:example/splash/splash_screen_view.dart';
 import 'package:flutter_arhitecture_helper/presentation/ui/mvvm/base_view_model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import 'splash_screen_model.dart';
+import 'splash_screen_view.dart';
+
 class SplashScreenViewModel
     extends BaseViewModel<SplashScreenModel, SplashScreenView> {
   SplashScreenViewModel(SplashScreenView view) : super(view);
--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:example/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     
